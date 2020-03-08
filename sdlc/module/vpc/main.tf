14441b6b0b1646992dd6aa94308e1b6f304380b8
Pablo Mella <pablo.mella.r@gmail.com>
2020-03-08 13:30:20 -0400

    upgrade vpc module version

--- sdlc/module/vpc/main.tf
+++ sdlc/module/vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 1.0.0"
+  version = "~> 2.1"
 
   project_id   = "${var.project}"
   network_name = "${var.env}"
